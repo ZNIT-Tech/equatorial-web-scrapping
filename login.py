87a65e01e74ec05d82229ab0f8d50d241d1a5626
enzorooschqueiroz <enzoroosch@gmail.com>
2025-04-10 16:55:30 -0300

    feat: enhance save_Credentials function with random user agent selection and adjusted delays

--- login.py
+++ login.py
@@ -10,21 +10,28 @@ DOWNLOAD_DIR = os.getenv("DOWNLOAD_DIR", "download")
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 os.makedirs(COOKIES_DIR, exist_ok=True)
 
+user_agents = [
+    "Mozilla/5.0 (iPad; CPU OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240"
+]
+
 def random_delay(min_delay=1.5, max_delay=3.5):
     time.sleep(random.uniform(min_delay, max_delay))
 
 async def save_Credentials(client_cpf_cnpj: str, senha: str, estado: str):
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True, slow_mo=80)  # mais natural
+        browser = await p.chromium.launch(headless=False, slow_mo=80)  # mais natural
         context = await browser.new_context(
-            user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+            #user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+            user_agent=random.choice(user_agents),
             accept_downloads=True,
             viewport={"width": 1280, "height": 800},
             locale="pt-BR",
             timezone_id="America/Sao_Paulo"
         )
         page = await context.new_page()
-
         try:
             await page.goto("https://pi.equatorialenergia.com.br/", timeout=60000)
             print("Página carregada.")
@@ -74,7 +81,7 @@ async def save_Credentials(client_cpf_cnpj: str, senha: str, estado: str):
 
                 await page.locator("#envia-identificador").click()
                 print("Segundo botão 'Entrar' clicado.")
-                await asyncio.sleep(10)
+                await asyncio.sleep(15)
 
                 if await page.locator("//span[contains(text(), 'Atenção')]").is_visible(timeout=3000):
                     print("Erro detectado, recarregando...")
